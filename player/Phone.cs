1dc506b6fa98b53582cb2f41cac61aac7906e4dd
DFelipehDEV <thediogopcm@gmail.com>
2024-11-03 10:41:55 +0000

    Room cams

--- player/Phone.cs
+++ player/Phone.cs
@@ -1,12 +1,22 @@
 using Godot;
-using System;
 
 public partial class Phone : Node3D
 {
 	private SpotLight3D light;
 	private CsgBox3D flashIcon;
-	private Material offMaterial;
-	private Material onMaterial;
+	private bool flash;
+	public bool Flash {
+		get {
+			return flash;
+		}
+		set {
+			flash = value;
+			light.Visible = value;
+
+			flashIcon.Material = value ? onMaterial : offMaterial;
+		}
+	}
+	private Material offMaterial, onMaterial;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -17,23 +27,5 @@ public partial class Phone : Node3D
 		// Store the original material and load the alternate material.
 		offMaterial = flashIcon.Material;
 		onMaterial = (Material)GD.Load("res://office/phone/ui/FlashlightOnIcon.tres");
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (Input.IsActionJustPressed("toggle_flash"))
-		{
-			light.Visible = !light.Visible;
-
-			if (light.Visible)
-			{
-				flashIcon.Material = onMaterial;
-			}
-			else
-			{
-				flashIcon.Material = offMaterial;
-			}
-		}
 	}
 }
