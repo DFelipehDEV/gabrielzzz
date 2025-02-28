0f986b36565686c1efa35c9fb838c3b66e97d466
DFelipehDEV <thediogopcm@gmail.com>
2025-02-28 16:09:59 +0000

    wip phone alarm

--- src/ricbedroom/phone/Nokia.cs
+++ src/ricbedroom/phone/Nokia.cs
@@ -1,19 +1,38 @@
 using Godot;
-using System;
 
 public partial class Nokia : Node3D
 {
 	private TextureProgressBar progress;
-	// Called when the node enters the scene tree for the first time.
+	private Area3D area;
+	private bool delayingAlarm = false;
+
 	public override void _Ready()
 	{
-		progress = GetNode<TextureProgressBar>("Progress/SubViewport/TextureProgressBar");
+		area = GetNode<Area3D>("Area");
+		progress = area.GetNode<TextureProgressBar>("Progress/SubViewport/ProgressBar");	
+		area.MouseEntered += OnMouseEntered;
+		area.MouseExited += OnMouseExited;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		progress.Value += 1;
+		
+
+		if (delayingAlarm) {
+			progress.Value += 30.0 * delta;
+		} else {
+			progress.Value -= 30.0 * delta;
+		}
 		progress.Value %= 100;
+		GD.Print(progress.Value);
+	}
+
+	public void OnMouseEntered() {
+		delayingAlarm = true;
+		GD.Print(delayingAlarm);
+	}
+
+	public void OnMouseExited() {
+		delayingAlarm = false;
 	}
 }
