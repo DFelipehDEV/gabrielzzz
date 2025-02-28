c299df05b677e11d15b167e46e1f516d695fe68b
DFelipehDEV <thediogopcm@gmail.com>
2025-02-28 14:57:26 +0000

    fixed modulate alpha

--- src/NightEnd/NightEnd.cs
+++ src/NightEnd/NightEnd.cs
@@ -1,16 +1,16 @@
 using Godot;
-using System;
 
 public partial class NightEnd : Control
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Modulate = new Color(Modulate.R, Modulate.G, Modulate.B, 0);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Modulate = new Color(Modulate.R, Modulate.G, Modulate.B, 1.0f * (float)delta);
+		Modulate = new Color(Modulate.R, Modulate.G, Modulate.B, Modulate.A + (1.0f * (float)delta));
 	}
 }
