e91ecd7adbfcdbfca100bace41f8194cfafa4447
DFelipehDEV <thediogopcm@gmail.com>
2025-03-12 14:17:38 +0000

    fixed nokia alarm not being delayed on mouse hover

--- src/ricbedroom/phone/Nokia.cs
+++ src/ricbedroom/phone/Nokia.cs
@@ -1,32 +1,61 @@
 using Godot;
 
-public partial class Nokia : Node3D
+public partial class Nokia : StaticBody3D
 {
-	private TextureProgressBar progress;
-	private bool delayingAlarm = false;
+    [Export]
+    public double AlarmIncreaseRate = 9.5;
 
-	public override void _Ready()
-	{
-		progress = GetNode<TextureProgressBar>("Progress/SubViewport/ProgressBar");	
-	}
+    [Export]
+    public double AlarmDecreaseRate = 3.5;
 
-	public override void _Process(double delta)
+    private TextureProgressBar progress;
+    private bool delayingAlarm = false;
+    private double alarmTimer = 100.0;
+
+    public override void _Ready()
+    {
+        progress = GetNode<TextureProgressBar>("Alarm/SubViewport/ProgressBar");
+    }
+
+    public override void _Process(double delta)
+    {
+        if (delayingAlarm)
+        {
+            alarmTimer += AlarmIncreaseRate * delta;
+        }
+        else
+        {
+            alarmTimer -= AlarmDecreaseRate * delta;
+        }
+        alarmTimer = Mathf.Clamp(alarmTimer, 0, 100);
+
+        progress.Value = alarmTimer;
+    }
+
+	public override void _Input(InputEvent @event)
 	{
-		if (delayingAlarm) {
-			progress.Value += 30.0 * delta;
-		} else {
-			progress.Value -= 30.0 * delta;
+		if (@event is InputEventMouseMotion mouseMotion)
+		{
+			var camera = GetViewport().GetCamera3D();
+			var from = camera.ProjectRayOrigin(mouseMotion.Position);
+			var to = from + camera.ProjectRayNormal(mouseMotion.Position) * 1000;
+
+			var spaceState = GetWorld3D().DirectSpaceState;
+			var result = spaceState.IntersectRay(new PhysicsRayQueryParameters3D
+			{
+				From = from,
+				To = to,
+				CollisionMask = 1
+			});
+
+			if (result.Count > 0 && result["collider"].AsGodotObject() == this)
+			{
+				delayingAlarm = true;
+			}
+			else
+			{
+				delayingAlarm = false;
+			}
 		}
-		progress.Value %= 100;
-		GD.Print(progress.Value);
-	}
-
-	public void OnMouseEntered() {
-		delayingAlarm = true;
-		GD.Print(delayingAlarm);
-	}
-
-	public void OnMouseExited() {
-		delayingAlarm = false;
 	}
 }
