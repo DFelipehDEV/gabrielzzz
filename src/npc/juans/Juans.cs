b2599cd43c59226309f4b7853d5134ec8bd85b4d
DFelipehDEV <thediogopcm@gmail.com>
2025-03-12 15:45:12 +0000

    improved Juans generator logic

--- src/npc/juans/Juans.cs
+++ src/npc/juans/Juans.cs
@@ -7,31 +7,34 @@ public partial class Juans : EnemyNPC
 	public Light3D flashLight;
 	public bool flashed = false;
 
+	private Generator generator;
+
 	public override void _Ready()
 	{
 		base._Ready();
+		generator = (Generator)GetTree().GetFirstNodeInGroup("generator");
+
 		faucetSound = GetNode<AudioStreamPlayer3D>("KitchenFaucet");
 		flashedSound = GetNode<AudioStreamPlayer3D>("FlashedSound");
 		officeSound = GetNode<AudioStreamPlayer3D>("OfficeSound");
 		flashLight = GetTree().Root.GetNode<Light3D>("Node3D/Player/Root/Phone/Flash");
 	}
 	public override void MovedToNewPosition(Node3D position) {
-		// Break generator
         if (position.IsInGroup("garage"))
         {
-            // 1 in 3 chance to break the generator
-            Random random = new Random();
-            int chance = random.Next(1, 4);
+			// Break generator
+			if (generator != null) 
+			{
+				// 1 in 3 chance to break the generator
+				Random random = new Random();
+				int chance = random.Next(1, 4);
 
-            if (chance == 1) // 1 in 3 chance
-            {
-                Generator generator = (Generator)position.GetParent().FindChild("Generator", false);
-                if (generator != null)
-                {
-                    generator.Broken = true;
-                    GD.Print("Generator has been broken");
-                }
-            }
+				if (chance == 1) // 1 in 3 chance
+				{
+					generator.Broken = true;
+					GD.Print("Generator has been broken");
+				}
+			}
 		}
 
 		if (position.IsInGroup("kitchen")) {
