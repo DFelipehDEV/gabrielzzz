9b783d1f0b64745861aa94eb7f5c66dd2b0bb6d4
DFelipehDEV <thediogopcm@gmail.com>
2025-03-14 09:10:27 +0000

    simon wakes up and sleeps on whether generator is broken or not

---
+++ src/npc/simon/Simon.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+public partial class Simon : EnemyNPC
+{
+	private bool awake = false;
+	private Generator generator;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		generator = (Generator)GetTree().GetFirstNodeInGroup("generator");
+		generator.Connect("GeneratorBroken", Callable.From(GeneratorBroken));
+		generator.Connect("GeneratorFixed", Callable.From(GeneratorFixed));
+	}
+
+	public override void _Process(double delta)
+	{
+		if (awake)
+			base._Process(delta);
+	}
+
+	public void GeneratorBroken() {
+		awake = true;
+		GD.Print(Name + " is now awake");
+	}
+
+	public void GeneratorFixed() {
+		awake = false;
+		GD.Print(Name + " is now asleep");
+	}
+}
