d8445bd39f23e39d0a7e22bd80ebce2bcface016
DFelipehDEV <thediogopcm@gmail.com>
2025-03-31 16:28:07 +0100

    cleanup vitel class

--- src/npc/vitel/Vitel.cs
+++ src/npc/vitel/Vitel.cs
@@ -1,15 +1,16 @@
 using Godot;
-using System;
 
 public partial class Vitel : EnemyNPC
 {
 	[Export]
 	private AnimatedSprite2D jumpScare;
+
 	[Export]
 	private AudioStreamPlayer jumpScareAudio;
-	
+
 	private double timeInOffice = 0.0;
 	private bool insideOffice = false;
+
 	public override void _Ready()
 	{
 		base._Ready();
@@ -19,10 +20,13 @@ public partial class Vitel : EnemyNPC
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
-		if (insideOffice){
+		if (insideOffice)
+		{
 			timeInOffice += 1.0 * delta;
-			if (timeInOffice > 5.00) {
-				if (!jumpScare.Visible) {
+			if (timeInOffice > 5.00)
+			{
+				if (!jumpScare.Visible)
+				{
 					jumpScare.Visible = true;
 					jumpScare.Play("JumpScare");
 					jumpScareAudio.Play();
@@ -31,13 +35,16 @@ public partial class Vitel : EnemyNPC
 		}
 	}
 
-	public void JumpScareFinished() {
+	public void JumpScareFinished()
+	{
 		OS.DelayMsec(1000);
 		GetTree().ChangeSceneToFile("res://game_over/GameOver.tscn");
 	}
 
-	public override void OnMovedToNewPosition(Node3D position) {
-		if (position.IsInGroup("office")) {
+	public override void OnMovedToNewPosition(Node3D position)
+	{
+		if (position.IsInGroup("office"))
+		{
 			insideOffice = true;
 		}
 	}
